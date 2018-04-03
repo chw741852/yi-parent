5b1cbdb63ca068b46e1fcf585687efc2f4c39604
caihw <caihongwei_2008@163.com>
2018-04-03 11:36:09 +0800

    application

--- d1/src/main/java/com/yi/d1/App.java
+++ d1/src/main/java/com/yi/d1/App.java
@@ -1,5 +1,6 @@
 package com.yi.d1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -9,12 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @RestController
 public class App {
+    @Value("${my.name}")
+    private String name;
+    @Value("${my.age}")
+    private int age;
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
     @GetMapping("/")
     public String index() {
-        return "Hello world!";
+        return "hello,i'm " + name + "," + age + " years old";
     }
 }
