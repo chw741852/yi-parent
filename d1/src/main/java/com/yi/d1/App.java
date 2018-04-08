e5549cd52aa350aaf25f7b68d1ead6684f787c48
caihw <caihongwei_2008@163.com>
2018-04-08 18:06:04 +0800

    spring cloud gateway

--- d1/src/main/java/com/yi/d1/App.java
+++ d1/src/main/java/com/yi/d1/App.java
@@ -4,16 +4,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @RestController
+@EnableDiscoveryClient
 public class App {
     @Value("${my.name}")
     private String name;
     @Value("${my.age}")
     private int age;
+    @Value("${foo.name:nothing}")
+    private String foo;
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
@@ -21,6 +25,11 @@ public class App {
 
     @GetMapping("/")
     public String index() {
-        return "hello,i'm " + name + "," + age + " years old";
+        return "hello,i'm " + name + "," + age + " years old.\nAnd " + foo;
+    }
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello World!";
     }
 }
--- discovery/src/main/java/com/yi/discovery/App.java
+++ discovery/src/main/java/com/yi/discovery/App.java
@@ -2,13 +2,13 @@ package com.yi.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @RestController
+@EnableEurekaServer
 public class App {
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
